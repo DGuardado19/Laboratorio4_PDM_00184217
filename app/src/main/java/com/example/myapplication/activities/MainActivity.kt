f842bc13e9b13e5b0acac1f448e26315bd4e9a45
Roberto <00162317@uca.edu.sv>
2019-04-05 14:00:23 -0600

    agregue nuevas variables que no conozco

--- app/src/main/java/com/example/myapplication/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.myapplication.activities
 
+import android.content.AbstractThreadedSyncAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import com.example.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var movieAdapter: MovieAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var movieList: Array<Movie> 0 ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
