d97430adcd8c59599266b6de6b45a6a3e9caeff9
Roberto <00162317@uca.edu.sv>
2019-04-05 14:06:58 -0600

    cree la data clas para manejar data

---
+++ app/src/main/java/com/example/myapplication/pojos/Movie.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.pojos
+
+data class Movie(
+    val Title:String ="N/A",
+    val Year:String = "N/A",
+    val Released:String = "N/A",
+    val Runtime:String = "N/A",
+    val Genre:String = "N/A",
+    val Director:String = "N/A",
+    val Actor:String = "N/A",
+    val PLot:String = "N/A",
+    val Languaje:String = "N/A",
+    val imbdRating:String = "N/A",
+    val Poster:String = "N/A"
+
+)
